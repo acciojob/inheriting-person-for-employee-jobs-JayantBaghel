15dd3891fe71c3d0fc622c6bab400898e9d97b7d
JayantBaghel <94191857+JayantBaghel@users.noreply.github.com>
2024-07-01 10:04:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // complete this js code
-function Person(name, age) {}
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-function Employee(name, age, jobTitle) {}
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
+}
 
+// Define the Employee class that extends the Person class
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age); // Call the parent class constructor
+        this.jobTitle = jobTitle;
+    }
+
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
+}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
